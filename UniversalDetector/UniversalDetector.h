4498c92829eac8427c60558829cc1c711650bb38
Francis Chong <francis@ignition.hk>
2013-05-07 11:36:03 +0800

    Initial Commit

---
+++ UniversalDetector/UniversalDetector.h
@@ -0,0 +1,13 @@
+//
+//  UniversalDetector.h
+//  UniversalDetector
+//
+//  Created by Francis Chong on 7/5/13.
+//  Copyright (c) 2013 Ignition Soft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UniversalDetector : NSObject
+
+@end
---
+++ UniversalDetector/UniversalDetector.m
@@ -0,0 +1,13 @@
+//
+//  UniversalDetector.m
+//  UniversalDetector
+//
+//  Created by Francis Chong on 7/5/13.
+//  Copyright (c) 2013 Ignition Soft. All rights reserved.
+//
+
+#import "UniversalDetector.h"
+
+@implementation UniversalDetector
+
+@end
