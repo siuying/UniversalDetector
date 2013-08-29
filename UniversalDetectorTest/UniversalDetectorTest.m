29dd80a8ca8f93c92a543d72b31db96a6c682ed1
Francis Chong <francis@ignition.hk>
2013-08-29 14:39:56 +0800

    Enable ISO-8859-2 and windows 1250 encoding detection
    This might cause false detection in Latin-1, but worth testing.

--- UniversalDetectorTest/UniversalDetectorTest.m
+++ UniversalDetectorTest/UniversalDetectorTest.m
@@ -37,48 +37,47 @@
     NSData* data = [UniversalDetectorTest dataWithFixtureFile:@"mingpao"];
     STAssertNotNil(data, nil);
     UniversalDetector* detector = [[UniversalDetector alloc] init];
-  
-    CFStringEncodings encoding;
-//    CFStringEncodings encoding = [detector encodingWithData:data];
-//    STAssertTrue(kCFStringEncodingInvalidId != encoding, nil);
-//    STAssertTrue(CFStringIsEncodingAvailable(encoding), nil);
-//    STAssertEquals(encoding, kCFStringEncodingBig5, nil);
-//    
-//    // http://www.jiji.com/jc/c?g=pol&k=2013050700380
-//    data = [UniversalDetectorTest dataWithFixtureFile:@"jijicom"];
-//    STAssertNotNil(data, nil);
-//    
-//    encoding = [detector encodingWithData:data];
-//    STAssertTrue(kCFStringEncodingInvalidId != encoding, nil);
-//    STAssertTrue(CFStringIsEncodingAvailable(encoding), nil);
-//    STAssertEquals(encoding, kCFStringEncodingEUC_JP, nil);
-//    
-//    // http://www.yomiuri.co.jp/national/news/20130507-OYT1T00237.htm?from=main5
-//    data = [UniversalDetectorTest dataWithFixtureFile:@"yomiuri"];
-//    STAssertNotNil(data, nil);
-//    
-//    encoding = [detector encodingWithData:data];
-//    STAssertTrue(kCFStringEncodingInvalidId != encoding, nil);
-//    STAssertTrue(CFStringIsEncodingAvailable(encoding), nil);
-//    STAssertEquals(encoding, kCFStringEncodingShiftJIS, nil);
-//    
-//    // http://www.theverge.com/2013/5/6/4303072/fitbit-flex-review
-//    data = [UniversalDetectorTest dataWithFixtureFile:@"theverge"];
-//    STAssertNotNil(data, nil);
-//    
-//    encoding = [detector encodingWithData:data];
-//    STAssertTrue(kCFStringEncodingInvalidId != encoding, nil);
-//    STAssertTrue(CFStringIsEncodingAvailable(encoding), nil);
-//    STAssertEquals(encoding, (CFStringEncodings) kCFStringEncodingUTF8, nil);
-//    
-//    // http://headline.2ch.net/bbynamazu/
-//    data = [UniversalDetectorTest dataWithFixtureFile:@"2ch"];
-//    STAssertNotNil(data, nil);
-//    
-//    encoding = [detector encodingWithData:data];
-//    STAssertTrue(kCFStringEncodingInvalidId != encoding, nil);
-//    STAssertTrue(CFStringIsEncodingAvailable(encoding), nil);
-//    STAssertEquals(encoding, (CFStringEncodings) kCFStringEncodingShiftJIS, nil);
+
+    CFStringEncodings encoding = [detector encodingWithData:data];
+    STAssertTrue(kCFStringEncodingInvalidId != encoding, nil);
+    STAssertTrue(CFStringIsEncodingAvailable(encoding), nil);
+    STAssertEquals(encoding, kCFStringEncodingBig5, nil);
+    
+    // http://www.jiji.com/jc/c?g=pol&k=2013050700380
+    data = [UniversalDetectorTest dataWithFixtureFile:@"jijicom"];
+    STAssertNotNil(data, nil);
+    
+    encoding = [detector encodingWithData:data];
+    STAssertTrue(kCFStringEncodingInvalidId != encoding, nil);
+    STAssertTrue(CFStringIsEncodingAvailable(encoding), nil);
+    STAssertEquals(encoding, kCFStringEncodingEUC_JP, nil);
+    
+    // http://www.yomiuri.co.jp/national/news/20130507-OYT1T00237.htm?from=main5
+    data = [UniversalDetectorTest dataWithFixtureFile:@"yomiuri"];
+    STAssertNotNil(data, nil);
+    
+    encoding = [detector encodingWithData:data];
+    STAssertTrue(kCFStringEncodingInvalidId != encoding, nil);
+    STAssertTrue(CFStringIsEncodingAvailable(encoding), nil);
+    STAssertEquals(encoding, kCFStringEncodingShiftJIS, nil);
+    
+    // http://www.theverge.com/2013/5/6/4303072/fitbit-flex-review
+    data = [UniversalDetectorTest dataWithFixtureFile:@"theverge"];
+    STAssertNotNil(data, nil);
+    
+    encoding = [detector encodingWithData:data];
+    STAssertTrue(kCFStringEncodingInvalidId != encoding, nil);
+    STAssertTrue(CFStringIsEncodingAvailable(encoding), nil);
+    STAssertEquals(encoding, (CFStringEncodings) kCFStringEncodingUTF8, nil);
+    
+    // http://headline.2ch.net/bbynamazu/
+    data = [UniversalDetectorTest dataWithFixtureFile:@"2ch"];
+    STAssertNotNil(data, nil);
+    
+    encoding = [detector encodingWithData:data];
+    STAssertTrue(kCFStringEncodingInvalidId != encoding, nil);
+    STAssertTrue(CFStringIsEncodingAvailable(encoding), nil);
+    STAssertEquals(encoding, (CFStringEncodings) kCFStringEncodingShiftJIS, nil);
     
     // http://m.money.pl/wiadomosci/artykul/sezon;wynikow;na;gpw;w;pelni;inwestorzy;czekaja;na;,196,0,1371588.html
     data = [UniversalDetectorTest dataWithFixtureFile:@"polish"];
@@ -105,47 +104,47 @@
 
     NSString* encoding = [detector encodingAsStringWithData:data];
     STAssertEqualObjects(encoding, @"Big5", nil);
-//
-//    // http://www.jiji.com/jc/c?g=pol&k=2013050700380
-//    data = [UniversalDetectorTest dataWithFixtureFile:@"jijicom"];
-//    STAssertNotNil(data, nil);
-//    
-//    encoding = [detector encodingAsStringWithData:data];
-//    STAssertEqualObjects(encoding, @"EUC-JP", nil);
-//    
-//    // http://www.yomiuri.co.jp/national/news/20130507-OYT1T00237.htm?from=main5
-//    data = [UniversalDetectorTest dataWithFixtureFile:@"yomiuri"];
-//    STAssertNotNil(data, nil);
-//    
-//    encoding = [detector encodingAsStringWithData:data];
-//    STAssertEqualObjects(encoding, @"Shift_JIS", nil);
-//    
-//    // http://www.theverge.com/2013/5/6/4303072/fitbit-flex-review
-//    data = [UniversalDetectorTest dataWithFixtureFile:@"theverge"];
-//    STAssertNotNil(data, nil);
-//    
-//    encoding = [detector encodingAsStringWithData:data];
-//    STAssertEqualObjects(encoding, @"UTF-8", nil);
-//    
-//    // http://headline.2ch.net/bbynamazu/
-//    data = [UniversalDetectorTest dataWithFixtureFile:@"2ch"];
-//    STAssertNotNil(data, nil);
-//    
-//    encoding = [detector encodingAsStringWithData:data];
-//    STAssertEqualObjects(encoding, @"Shift_JIS", nil);
-//    
+
+    // http://www.jiji.com/jc/c?g=pol&k=2013050700380
+    data = [UniversalDetectorTest dataWithFixtureFile:@"jijicom"];
+    STAssertNotNil(data, nil);
+    
+    encoding = [detector encodingAsStringWithData:data];
+    STAssertEqualObjects(encoding, @"EUC-JP", nil);
+    
+    // http://www.yomiuri.co.jp/national/news/20130507-OYT1T00237.htm?from=main5
+    data = [UniversalDetectorTest dataWithFixtureFile:@"yomiuri"];
+    STAssertNotNil(data, nil);
+    
+    encoding = [detector encodingAsStringWithData:data];
+    STAssertEqualObjects(encoding, @"Shift_JIS", nil);
+    
+    // http://www.theverge.com/2013/5/6/4303072/fitbit-flex-review
+    data = [UniversalDetectorTest dataWithFixtureFile:@"theverge"];
+    STAssertNotNil(data, nil);
+    
+    encoding = [detector encodingAsStringWithData:data];
+    STAssertEqualObjects(encoding, @"UTF-8", nil);
+    
+    // http://headline.2ch.net/bbynamazu/
+    data = [UniversalDetectorTest dataWithFixtureFile:@"2ch"];
+    STAssertNotNil(data, nil);
+    
+    encoding = [detector encodingAsStringWithData:data];
+    STAssertEqualObjects(encoding, @"Shift_JIS", nil);
+
     // http://m.money.pl/wiadomosci/artykul/sezon;wynikow;na;gpw;w;pelni;inwestorzy;czekaja;na;,196,0,1371588.html
     data = [UniversalDetectorTest dataWithFixtureFile:@"polish"];
     STAssertNotNil(data, nil);
     
     encoding = [detector encodingAsStringWithData:data];
-    STAssertEqualObjects(encoding, @"ISO-8859-2", nil);
-//
-//    // pure ascii encoding
-//    data = [UniversalDetectorTest dataWithFixtureFile:@"ascii"];
-//    STAssertNotNil(data, nil);
-//    encoding = [detector encodingAsStringWithData:data];
-//    STAssertEqualObjects(encoding, @"", nil);
+    STAssertEqualObjects(encoding, @"ISO-8859-2", nil);   
+
+    // pure ascii encoding
+    data = [UniversalDetectorTest dataWithFixtureFile:@"ascii"];
+    STAssertNotNil(data, nil);
+    encoding = [detector encodingAsStringWithData:data];
+    STAssertEqualObjects(encoding, @"", nil);
 }
 
 @end
